4954dab8a48b6a3852e56ae1bf3fcf1536d37688
Paulo Gaia <paulo.gaia@flapper.aero>
2025-01-30 19:16:47 -0300

    test(travel-order): add factory travel order

---
+++ onfly-app/database/factories/TravelOrderFactory.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\TravelOrder;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class TravelOrderFactory extends Factory
+{
+    protected $model = TravelOrder::class;
+
+    public function definition()
+    {
+        return [
+            'user_id'    => \App\Models\User::factory(),
+            'destination'=> $this->faker->city,
+            'departure_date' => $this->faker->dateTimeBetween('now', '+1 month'),
+            'return_date' => $this->faker->dateTimeBetween('+2 months', '+3 months'),
+            'status'     => 'solicitado',
+        ];
+    }
+}
